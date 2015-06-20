5245bf307df3af9266536af21e13df1145c6d5f1
Ahmed Magdy <ahmed.magdy40@gmail.com>
2015-06-20 05:42:28 +0200

    writing the skeleton javascript actions

--- website/frontend/static/js/custom.js
+++ website/frontend/static/js/custom.js
@@ -1,17 +1,36 @@
 // preloader
-$(window).load(function(){
+$(window).load(function () {
     $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
-$(function() {
+$(function () {
     new WOW().init();
     $('.templatemo-nav').singlePageNav({
-    	offset: 70
+        offset: 70
     });
 
     /* Hide mobile menu after clicking on a link
-    -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
+     -----------------------------------------------*/
+    $('.navbar-collapse a').click(function () {
         $(".navbar-collapse").collapse('hide');
     });
-})
+
+    /*
+     * FACEBOOK SENTIMENT ANALYSIS
+     * */
+
+//    onchange file-input
+    $('#file-button').on('click', function (event) {
+        console.log("file_input");
+    });
+
+//    onchange sentence-input
+    $('#sentence-button').on('click', function (event) {
+        console.log("sentence_button");
+    });
+
+//    onchange post-url-input
+    $('#post-url-button').on('click', function (event) {
+        console.log("post_url_button");
+    });
+});
