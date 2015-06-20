93ef1cd33c8df50f58ee49d550f140a3bb72add2
Ahmed Magdy <ahmed.magdy40@gmail.com>
2015-06-20 05:14:14 +0200

    adding the template

--- website/frontend/templatetags/frontend_layout.py
+++ website/frontend/templatetags/frontend_layout.py
@@ -2,13 +2,32 @@ from django import template
 
 register = template.Library()
 
+
 @register.inclusion_tag('_layout/head.html')
 def layout_head():
     return {}
 
+
+@register.inclusion_tag('_layout/footer.html')
+def layout_footer():
+    return {}
+
+
 @register.inclusion_tag('_layout/scripts.html')
 def load_scripts():
     return {}
+
+
+
+
+@register.inclusion_tag('_layout/sections/contact.html')
+def section_contact():
+    return {}
+
+
+
+
+
 
 #
 
